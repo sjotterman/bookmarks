818987db2e0719ac39213a0c04489c23c4e602a7
Samuel Otterman <samuel.otterman@brandingbrand.com>
2021-11-15 20:57:02 -0500

    create bookmarks file if it doesn't exist

--- internal/ui/commands.go
+++ internal/ui/commands.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"bufio"
+	"errors"
 	"log"
 	"os"
 
@@ -9,9 +10,24 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+func (m Model) createEmptySaveFileCmd(filename string) tea.Cmd {
+	return func() tea.Msg {
+		emptyFile, err := os.Create(filename)
+		if err != nil {
+			log.Fatalf("Could not create file: %s", err)
+		}
+		emptyFile.Close()
+		return emptyFileCreatedMsg{emptyFile.Name()}
+	}
+}
+
 func (m Model) loadBookmarksCmd() tea.Cmd {
 	return func() tea.Msg {
 		saveFilePath := m.saveFilePath
+		_, err := os.Stat(saveFilePath)
+		if errors.Is(err, os.ErrNotExist) {
+			return saveFileNotExistsMsg{saveFilePath}
+		}
 		file, err := os.Open(saveFilePath)
 		if err != nil {
 			log.Fatal(err)
--- internal/ui/update.go
+++ internal/ui/update.go
@@ -1,18 +1,34 @@
 package ui
 
 import (
+	"fmt"
+
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type updateBookmarkListMsg struct{ items []list.Item }
+type saveFileNotExistsMsg struct{ filename string }
+type emptyFileCreatedMsg struct{ filename string }
 
 func (m *Model) handleXKeyPress(cmds *[]tea.Cmd) {
 	m.list.Title = "Pressed a button!"
 }
 
 func (m *Model) handleUpdateBookmarksListMsg(msg updateBookmarkListMsg, cmds *[]tea.Cmd) {
+	cmd := m.list.NewStatusMessage("Loaded bookmarks!")
+	*cmds = append(*cmds, cmd)
 	m.list.SetItems(msg.items)
+}
+
+func (m *Model) handleCreateEmptyFileMsg(msg saveFileNotExistsMsg, cmds *[]tea.Cmd) {
+	*cmds = append(*cmds, m.createEmptySaveFileCmd(msg.filename))
+}
+
+func (m *Model) handleEmptyFileCreatedMsg(msg emptyFileCreatedMsg, cmds *[]tea.Cmd) {
+	statusMsg := fmt.Sprintf("Created %s", msg.filename)
+	cmd := m.list.NewStatusMessage(statusMsg)
+	*cmds = append(*cmds, cmd)
 }
 
 // handleWindowSizeMsg is received whenever the window size changes.
@@ -37,6 +53,10 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case updateBookmarkListMsg:
 		m.handleUpdateBookmarksListMsg(msg, &cmds)
+	case saveFileNotExistsMsg:
+		m.handleCreateEmptyFileMsg(msg, &cmds)
+	case emptyFileCreatedMsg:
+		m.handleEmptyFileCreatedMsg(msg, &cmds)
 
 	}
 	m.list, cmd = m.list.Update(msg)
